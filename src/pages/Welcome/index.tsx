c773fcf7070a59dd14d0b7bef9d21899a58837df
Daniel Macedo <smacedo.daniel@gmail.com>
2021-12-07 18:23:18 -0300

    Create Login card

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,9 +1,55 @@
-import { Card } from "@mui/material";
+import { Button, Card, Stack, TextField } from "@mui/material";
+import { ChangeEvent, useState } from "react";
 
 const Welcome = () => {
+  const [userName, setUserName] = useState<string>();
+  const [hasUserNameError, setHasUserNameError] = useState<boolean>();
+
+  const [password, setPassword] = useState<string>();
+  const [hasPasswordError, setHasPasswordError] = useState<boolean>(false);
+
+  const handleChangeUserName = (event: ChangeEvent<HTMLInputElement>) => {
+    const newUserName = event.target.value;
+
+    setUserName(newUserName);
+
+    setHasUserNameError(newUserName.length < 3);
+  };
+
+  const handleChangePassword = (event: ChangeEvent<HTMLInputElement>) => {
+    const newPassword = event.target.value;
+
+    setPassword(newPassword);
+
+    setHasPasswordError(newPassword.length < 3);
+  };
+
   return (
-    <Card sx={{ minWidth: 275 }}>
-      <h1>Hello World</h1>
+    <Card sx={{ padding: 4, width: "auto" }}>
+      <Stack sx={{ margin: 0 }}>
+        <TextField
+          value={userName ?? ""}
+          error={hasUserNameError}
+          helperText={
+            hasUserNameError && "User must have more than 3 characters"
+          }
+          placeholder="User"
+          sx={{ paddingBottom: 2 }}
+          onChange={handleChangeUserName}
+        />
+        <TextField
+          value={password ?? ""}
+          error={hasPasswordError}
+          helperText={
+            hasPasswordError && "Password must have more than 3 characters"
+          }
+          placeholder="Password"
+          sx={{ paddingBottom: 4 }}
+          onChange={handleChangePassword}
+        />
+
+        <Button variant="contained">Login</Button>
+      </Stack>
     </Card>
   );
 };
