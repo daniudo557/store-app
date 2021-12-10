d26c7762dddb7b489cf060bb44a1b57cf023f488
Daniel Macedo <smacedo.daniel@gmail.com>
2021-12-09 22:22:19 -0300

    Update productsCard style

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -4,32 +4,60 @@ import {
   CardActions,
   CardContent,
   CardMedia,
+  Rating,
   Stack,
   Typography,
 } from "@mui/material";
+import { Product } from "src/domains/Product";
+import CategoryTag from "../CategoryTag";
 import "./ProductCard.scss";
 
-const ProductCard = () => {
+interface ProductCardProps {
+  product: Product;
+}
+
+const ProductCard = (props: ProductCardProps) => {
+  const { product } = props;
+
   return (
     <Card className="card">
       <CardMedia
         component="img"
         alt="todo list"
         height="200"
-        image="https://images.pexels.com/photos/1226398/pexels-photo-1226398.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+        image={product.image}
       />
       <CardContent>
         <Stack direction="row" justifyContent="space-between">
-          <Stack direction="row" alignItems="flex-end">
-            <Typography variant="h5" component="div">
-              Titulo
+          <Stack
+            direction="row"
+            alignItems="flex-end"
+            className="title-container"
+          >
+            <Typography variant="h5" component="div" className="title">
+              {product.title}
             </Typography>
           </Stack>
         </Stack>
 
+        <CategoryTag category={product.category} />
+
+        <Rating value={product.rating.rate} readOnly />
+
         <Typography variant="h6" color="text.secondary" sx={{ marginTop: 3 }}>
-          Descricao
+          $ {product.price}
         </Typography>
+
+        <div className="-container">
+          <Typography
+            variant="h6"
+            color="text.secondary"
+            sx={{ marginTop: 3 }}
+            className="description"
+          >
+            {product.description}
+          </Typography>
+        </div>
       </CardContent>
       <CardActions>
         <Button size="small">Add to cart</Button>
