a3e04ceb68fbd047a8f902f760e747e66aa56de7
Daniel Macedo <smacedo.daniel@gmail.com>
2021-12-09 22:20:16 -0300

    Include request to products on Products page

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,7 +1,12 @@
 import { Grid } from "@mui/material";
 import ProductCard from "src/components/ProductCard";
+import { useProduct } from "src/hooks/useProduct";
 
 const Products = () => {
+  const { productList, isLoading } = useProduct();
+
+  if (isLoading) return <h1>Loading...</h1>;
+
   return (
     <div
       style={{
@@ -15,9 +20,9 @@ const Products = () => {
           spacing={{ xs: 2, md: 3 }}
           columns={{ xs: 4, sm: 8, md: 12, xl: 1 }}
         >
-          {Array.from(Array(6)).map((_, index) => (
+          {productList?.map((product, index) => (
             <Grid item xs={12} sm={4} md={4} key={index}>
-              <ProductCard />
+              <ProductCard product={product} />
             </Grid>
           ))}
         </Grid>
