05731b6fabfb541a8e175ddea947041ed635a361
Daniel Macedo <smacedo.daniel@gmail.com>
2021-12-09 22:18:51 -0300

    Include request to products

---
+++ src/hooks/useProduct.ts
@@ -0,0 +1,60 @@
+import {  useQuery } from "react-query";
+import { useParams } from "react-router";
+import {
+  fetchProduct as fetchProductService,
+} from "src/services/productService";
+
+export const useProduct = () => {
+  // const queryClient = useQueryClient();
+  const params: { id?: string } = useParams();
+
+  const { data: productList, isLoading: isLoadingProductList } = useQuery(
+    "productList",
+    fetchProductService,
+    {
+      enabled: !params.id,
+    }
+  );
+
+  // const { data: product, isLoading: isLoadingProduct } = useQuery(
+  //   "product",
+  //   () => findProductService(params.id as string),
+  //   {
+  //     enabled: !!params.id,
+  //   }
+  // );
+
+  // const { mutate: createProduct } = useMutation(createProductService, {
+  //   onSuccess: () => {
+  //     queryClient.invalidateQueries("productList");
+  //     queryClient.invalidateQueries("product");
+  //   },
+  // });
+
+  // const { mutate: removeProduct } = useMutation(deleteProductService, {
+  //   onSuccess: () => {
+  //     queryClient.invalidateQueries("productList");
+  //     queryClient.invalidateQueries("product");
+  //   },
+  // });
+
+  // const { mutate: updateProduct } = useMutation(updateProductService, {
+  //   onSuccess: () => {
+  //     queryClient.invalidateQueries("productList");
+  //     queryClient.invalidateQueries("product");
+  //   },
+  // });
+
+  // return {
+  //   product,
+  //   isLoading: isLoadingProduct || isLoadingProductList,
+  //   productList,
+  //   createProduct,
+  //   removeProduct,
+  //   updateProduct,
+  // };
+  return {
+    isLoading: isLoadingProductList,
+    productList,
+  };
+};
