aa4f364f6f6df8e5bc766a748592cac43a448f67
Daniel Macedo <smacedo.daniel@gmail.com>
2021-12-10 13:40:22 -0300

    Implement dark mode

--- src/components/Appbar/index.tsx
+++ src/components/Appbar/index.tsx
@@ -1,4 +1,6 @@
 import { AccountCircle, Menu as MenuIcon } from "@mui/icons-material";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
 import {
   AppBar as AppbarMUI,
   IconButton,
@@ -9,6 +11,7 @@ import {
   Typography,
   useScrollTrigger,
 } from "@mui/material";
+import { useTheme } from "@mui/system";
 import { useCallback, useMemo, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { Routes } from "src/configs/routes";
@@ -33,8 +36,14 @@ function HideOnScroll(props: HideOnScrollProps) {
   );
 }
 
-const Appbar = () => {
+interface AppbarProps {
+  prefersDarkMode: boolean;
+  setPrefersDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Appbar = ({ prefersDarkMode, setPrefersDarkMode }: AppbarProps) => {
   const history = useHistory();
+  const theme = useTheme();
 
   const handleClick = () => history.push(Routes.ROOT);
 
@@ -57,6 +66,12 @@ const Appbar = () => {
 
     history.push(Routes.ROOT);
   }, [handleClose, history]);
+
+  const handleChangeTheme = useCallback(() => {
+    console.log({ theme });
+
+    setPrefersDarkMode((prevMode) => !prevMode);
+  }, [setPrefersDarkMode, theme]);
 
   return (
     <>
@@ -91,33 +106,46 @@ const Appbar = () => {
                 Store App
               </Typography>
 
-              {isUserLogged && (
-                <div>
-                  <IconButton
-                    size="large"
-                    aria-label="account of current user"
-                    aria-controls="menu-appbar"
-                    aria-haspopup="true"
-                    onClick={handleMenu}
-                    color="inherit"
-                  >
-                    <AccountCircle />
-                  </IconButton>
-                  <Menu
-                    id="menu-appbar"
-                    anchorEl={anchorEl}
-                    anchorOrigin={{
-                      vertical: "bottom",
-                      horizontal: "right",
-                    }}
-                    keepMounted
-                    open={Boolean(anchorEl)}
-                    onClose={handleClose}
-                  >
-                    <MenuItem onClick={handleLogout}>Logout</MenuItem>
-                  </Menu>
-                </div>
-              )}
+              <div style={{ display: "flex" }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleChangeTheme}
+                  color="inherit"
+                >
+                  {prefersDarkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+                </IconButton>
+
+                {isUserLogged && (
+                  <div>
+                    <IconButton
+                      size="large"
+                      aria-label="account of current user"
+                      aria-controls="menu-appbar"
+                      aria-haspopup="true"
+                      onClick={handleMenu}
+                      color="inherit"
+                    >
+                      <AccountCircle />
+                    </IconButton>
+                    <Menu
+                      id="menu-appbar"
+                      anchorEl={anchorEl}
+                      anchorOrigin={{
+                        vertical: "bottom",
+                        horizontal: "right",
+                      }}
+                      keepMounted
+                      open={Boolean(anchorEl)}
+                      onClose={handleClose}
+                    >
+                      <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                    </Menu>
+                  </div>
+                )}
+              </div>
             </div>
           </Toolbar>
         </AppbarMUI>
