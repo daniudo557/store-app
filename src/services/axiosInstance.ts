e039776f234247cd578a2a06d8bd863a85cdf7a2
Daniel Macedo <smacedo.daniel@gmail.com>
2021-12-13 00:56:36 -0300

    Include interceptors on axios instance

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -4,4 +4,14 @@ const baseURL = "http://localhost:3001";
 
 const axiosInstance = axios.create({ baseURL });
 
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default axiosInstance;
