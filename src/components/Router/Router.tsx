ec38ef5418bebcaf7d1fbf8498af0aea0345d8ce
Daniel Macedo <smacedo.daniel@gmail.com>
2021-12-14 00:52:41 -0300

    Create validation to user cant access private routes

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,16 +1,35 @@
-import { Route, Switch } from "react-router";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { Redirect, Route, Switch } from "react-router";
+import { Routes } from "src/configs/routes";
+import { User } from "src/domains/User";
 import { useRoute } from "src/hooks/useRoute";
+import { RootState } from "src/redux/store";
 
 const Router = () => {
   const { routes } = useRoute();
+  const { user } = useSelector<RootState, { user?: User }>(
+    (state) => state?.user
+  );
+
+  const isUserLogged = useMemo(() => !!user, [user]);
 
   return (
     <Switch>
       {routes.map((route) => {
-        const { Component, url, key } = route;
+        const { Component, url, key, protectedRoute } = route;
+
+        const checkProtectedRoute = () => {
+          if (!isUserLogged) {
+            return <Redirect to={Routes.ROOT} />;
+          }
+
+          return <Component />;
+        };
+
         return (
           <Route key={key} path={url} exact>
-            <Component />
+            {protectedRoute ? checkProtectedRoute : <Component />}
           </Route>
         );
       })}
